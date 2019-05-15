fa85115511aa0f8971bf9753e86724bd25f63bc6
imolinav <imolinav@ieslavereda.es>
2019-05-15 13:08:40 +0200

    Filtro log

--- actualizar_log.php
+++ actualizar_log.php
@@ -2,16 +2,28 @@
 require_once "metodos.php";
 if (isset($_POST['log_query'])) {
     $datos = json_decode($_POST['log_query'], true);
-    if ($datos['f_inicio'] == "") {
-        $stmt = $conexion->prepare("SELECT * FROM log WHERE fecha<=:f_fin ORDER BY fecha DESC, hora DESC");
+    $fecha = "";
+    $habitacion="";
+    if ($datos['f_inicio'] == "" && $datos['f_fin'] != "") {
+        $fecha = "fecha<=:f_fin";
         $parameters = [':f_fin' => $datos['f_fin']];
-    } else if ($datos['f_fin'] == "") {
-        $stmt = $conexion->prepare("SELECT * FROM log WHERE fecha>=:f_inicio ORDER BY fecha DESC, hora DESC");
+    } else if ($datos['f_fin'] == "" && $datos['f_inicio'] != "") {
+        $fecha = "fecha>=:f_inicio";
         $parameters = [':f_inicio' => $datos['f_inicio']];
-    } else {
-        $stmt = $conexion->prepare("SELECT * FROM log WHERE fecha>=:f_inicio && fecha<=:f_fin ORDER BY fecha DESC, hora DESC");
+    } else if($datos['f_inicio']!="" && $datos['f_fin']!="") {
+        $fecha = "fecha>=:f_inicio && fecha<=:f_fin";
         $parameters = [':f_inicio' => $datos['f_inicio'], ':f_fin' => $datos['f_fin']];
     }
+    if ($datos['habitacion']!="") {
+        if($datos['f_inicio'] != "" || $datos['f_fin'] != "") {
+            $habitacion = " AND habitacion = :habitacion ";
+            $parameters[':habitacion'] = $datos['habitacion'];
+        } else {
+            $habitacion = "habitacion = :habitacion";
+            $parameters = [':habitacion' => $datos['habitacion']];
+        }
+    }
+    $stmt = $conexion->prepare("SELECT * FROM log WHERE ".$fecha.$habitacion." ORDER BY fecha DESC, hora DESC");
     $stmt->execute($parameters);
     $new_log = $stmt->fetchAll(PDO::FETCH_ASSOC);
     if (!empty($new_log)) :?>
--- historial.php
+++ historial.php
@@ -2,6 +2,11 @@
 include "metodos.php";
 $logs = $usuario->getLogs($conexion, 50);
 $logs_excel = $usuario->getLogs($conexion, 500);
+
+$stmt_log_hab = $conexion->prepare("SELECT habitacion FROM log WHERE usuario_email = :email GROUP BY habitacion");
+$parameters = [':email'=>$usuario->getEmail()];
+$stmt_log_hab->execute($parameters);
+$hab_logs = $stmt_log_hab->fetchAll(PDO::FETCH_ASSOC);
 if (isset($_POST['export_xls'])) {
     $filename = "log_dispositivos.xls";
     header("Content-Type: application/vnd.ms-excel");
